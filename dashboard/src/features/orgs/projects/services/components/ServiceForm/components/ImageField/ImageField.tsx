4e9d8dc1a22efb982097f1cf24c717b27a917c57
robertkasza <robert.kasza@bishop-co.com>
2026-05-14 20:43:02 +0200

    fix(dashboard): Migrate services page icons

--- dashboard/src/features/orgs/projects/services/components/ServiceForm/components/ImageField/ImageField.tsx
+++ dashboard/src/features/orgs/projects/services/components/ServiceForm/components/ImageField/ImageField.tsx
@@ -1,11 +1,10 @@
 import { inputBaseClasses } from '@mui/material';
 import { useTheme } from '@mui/system';
+import { ExternalLink as ArrowSquareOutIcon, InfoIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { Box } from '@/components/ui/v2/Box';
 import { Input } from '@/components/ui/v2/Input';
-import { ArrowSquareOutIcon } from '@/components/ui/v2/icons/ArrowSquareOutIcon';
-import { InfoIcon } from '@/components/ui/v2/icons/InfoIcon';
 import { Link } from '@/components/ui/v2/Link';
 import { Text } from '@/components/ui/v2/Text';
 import { Tooltip } from '@/components/ui/v2/Tooltip';
@@ -136,11 +135,7 @@ export default function ImageField({
                   </span>
                 }
               >
-                <InfoIcon
-                  aria-label="Info"
-                  className="h-4 w-4"
-                  color="primary"
-                />
+                <InfoIcon aria-label="Info" className="h-4 w-4 text-primary" />
               </Tooltip>
             </Box>
           }
